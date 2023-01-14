70b74a39fa9385b3bdb2efd0682451c9db2374fc
edwardma33 <edwardma33@icloud.com>
2023-01-14 02:37:55 -0500

    Ok fr this time

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
-from main.routes import main
-from extensions import mongo
 
-def create_app():
-    app = Flask(__name__)
 
-    app.config['MONGO_URI'] = 'mongodb+srv://edwardma33:Delaware33@cluster0.hp6ofba.mongodb.net/?retryWrites=true&w=majority'
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb+srv://edwardma33:Delaware33@cluster0.hp6ofba.mongodb.net/?retryWrites=true&w=majority'
+mongo = PyMongo(app)
 
-    mongo.init_app(app)
+@app.route('/')
+def home_page():
+    dashboard = mongo.db.tasks.find({})
 
-    app.register_blueprint(main)
+    return dashboard
 
-    return app
+
+
+
